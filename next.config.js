5bd0fbfee4022d1712e264de6ad23819f571cdd1
Pha6ha007 <photobp2019@gmail.com>
2026-04-01 18:19:45 +0300

    fix: explicitly set X-Frame-Options ALLOWALL to override Next.js default

--- next.config.js
+++ next.config.js
@@ -18,6 +18,7 @@ const nextConfig = {
       {
         source: '/(.*)',
         headers: [
+          { key: 'X-Frame-Options', value: 'ALLOWALL' },
           { key: 'Content-Security-Policy', value: "frame-ancestors *" },
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
